752a855e73b53295c6854cd440923e1c93cf8a91
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-10-25 09:12:35 -0500

    init commit

---
+++ src/main/java/com/weobsocketintro/websocketintro/WebsocketintroApplication.java
@@ -0,0 +1,13 @@
+package com.weobsocketintro.websocketintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketintroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weobsocketintro/websocketintro/WebsocketintroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weobsocketintro.websocketintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
